4c023c0c45170760b5faed9525a97d1a5e20765b
Om Narayan Trivedi <omtrivedi.7824@gmail.com>
2023-11-09 15:01:25 +0530

    Easy

--- DecompressRun-LengthEncodedList.cpp
+++ DecompressRun-LengthEncodedList.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int>answer;
+        for(int i = 0 ; i < nums.size() - 1 ; i+=2){
+            for(int j = 0 ; j < nums[i] ; j++){
+                answer.push_back(nums[i+1]);
+            }
+        }
+        return answer;
+    }
+};
