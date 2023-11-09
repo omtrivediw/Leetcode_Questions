d6378ab9eb77099d69b40e8d51541848edb4ca04
Om Narayan Trivedi <omtrivedi.7824@gmail.com>
2023-11-09 13:34:05 +0530

    Easy

---
+++ ConcatenationOfArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int>ans;
+        int size = nums.size();
+        for(int i = 0 ; i < size ; i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i = 0 ; i < size ; i++){
+            ans.push_back(ans[i]);
+        }
+        return ans;
+    }
+};
