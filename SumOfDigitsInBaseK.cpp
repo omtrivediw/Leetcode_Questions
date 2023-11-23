67c1b07cc13952579f3463fe4080e299aab49967
Om Narayan Trivedi <omtrivedi.7824@gmail.com>
2023-11-23 18:00:44 +0530

    Easy

---
+++ SumOfDigitsInBaseK.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int count = 0;
+        while(n != 0){
+            count += n % k;
+            n = n / k;
+        }
+        return count;
+    }
+};
