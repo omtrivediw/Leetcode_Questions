36d17d79ca9d6b0a70139dc34f86d2f4a50c619a
Om Narayan Trivedi <omtrivedi.7824@gmail.com>
2023-12-11 14:48:34 +0530

    Easy

---
+++ AverageValueofEvenNumbersThatAreDivisiblebyThree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int averageValue(vector<int>& nums) {
+        int count = 0;
+        int sum = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] % 2 == 0 and nums[i] % 3 == 0){
+                count++;
+                sum+=nums[i];
+            }
+        }
+        if(count == 0) return 0;
+        return sum / count;
+    }
+};
